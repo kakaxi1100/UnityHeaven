cc2f5c5bc7dfdd53bdff27755366cb7069692058
kakaxi1100 <JuLi@APSHADESK637.aaa.ad.ea.com>
2015-08-31 17:57:23 +0800

    [C#] add file merge and cut project

---
+++ Practice/FileMergeAndCut/FileMergeAndCut/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace FileMergeAndCut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.openFileTB = new System.Windows.Forms.TextBox();
+            this.saveDirctTB = new System.Windows.Forms.TextBox();
+            this.unitCombo = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(174, 21);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(75, 23);
+            this.openBtn.TabIndex = 1;
+            this.openBtn.Text = "Open";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(174, 55);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 3;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 75);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(133, 30);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "By Size";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "By Count";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 181);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(85, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(31, 236);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(239, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // openFileTB
+            // 
+            this.openFileTB.Location = new System.Drawing.Point(31, 21);
+            this.openFileTB.Name = "openFileTB";
+            this.openFileTB.ReadOnly = true;
+            this.openFileTB.Size = new System.Drawing.Size(100, 20);
+            this.openFileTB.TabIndex = 8;
+            // 
+            // saveDirctTB
+            // 
+            this.saveDirctTB.Location = new System.Drawing.Point(31, 57);
+            this.saveDirctTB.Name = "saveDirctTB";
+            this.saveDirctTB.ReadOnly = true;
+            this.saveDirctTB.Size = new System.Drawing.Size(100, 20);
+            this.saveDirctTB.TabIndex = 9;
+            // 
+            // unitCombo
+            // 
+            this.unitCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.unitCombo.FormattingEnabled = true;
+            this.unitCombo.Items.AddRange(new object[] {
+            "Byte",
+            "KB",
+            "MB",
+            "GB"});
+            this.unitCombo.Location = new System.Drawing.Point(123, 179);
+            this.unitCombo.Name = "unitCombo";
+            this.unitCombo.Size = new System.Drawing.Size(121, 21);
+            this.unitCombo.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 284);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.unitCombo);
+            this.Controls.Add(this.saveDirctTB);
+            this.Controls.Add(this.openFileTB);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.openBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox openFileTB;
+        private System.Windows.Forms.TextBox saveDirctTB;
+        private System.Windows.Forms.ComboBox unitCombo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Practice/FileMergeAndCut/FileMergeAndCut/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileMergeAndCut
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFileDlog;
+        private FileStream openFileStream;
+
+        private FolderBrowserDialog saveFolderDlog;
+        public Form1()
+        {
+            InitializeComponent();
+            unitCombo.SelectedIndex = 2;
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDlog == null)
+            {
+                openFileDlog = new OpenFileDialog();
+                openFileDlog.RestoreDirectory = true;
+            }
+
+            if(openFileDlog.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fileInfo = new FileInfo(openFileDlog.FileName);
+                openFileTB.Text = fileInfo.FullName;
+
+                openFileStream = fileInfo.Open(FileMode.Open, FileAccess.Read);
+
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            if (saveFolderDlog == null)
+            {
+                saveFolderDlog = new FolderBrowserDialog();
+            }
+            if(saveFolderDlog.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryInfo dirctInfo = new DirectoryInfo(saveFolderDlog.SelectedPath);
+                saveDirctTB.Text = dirctInfo.FullName;
+            }
+        }
+    }
+}
