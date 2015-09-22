d0cfb308aae534d3bb343cb90f0647857af42aa7
kakaxi2222 <aresleecool@163.com>
2015-09-22 23:36:11 +0800

    [C#] add framework

--- Practice/FrameWork/RPG/Game.cs
+++ Practice/FrameWork/RPG/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,51 @@ namespace RPG
 {
     class Game
     {
+        Bitmap mBmp;
+        Graphics mDevice;
+        public Game()
+        {
+
+        }
+
+        public Bitmap LoadBitmap(string fileName)
+        {
+            Bitmap bmp = null;
+            try
+            {
+                bmp = new Bitmap(fileName);
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+            return bmp;
+        }
+
+        public Bitmap CutBitmap(ref Bitmap source, int x, int y, int w, int h)
+        {
+            Bitmap temp = new Bitmap(w, h);
+            Graphics g = Graphics.FromImage(temp);
+            g.DrawImage(source, x, y, w, h);
+            return temp;
+        }
+        public Bitmap CutBitmap(string fileName, int x, int y, int w, int h)
+        {
+            Bitmap source = LoadBitmap(fileName);
+            Bitmap temp = new Bitmap(w, h);
+            Graphics g = Graphics.FromImage(temp);
+            g.DrawImage(source, x, y, w, h);
+            return temp;
+        }
+
+        public void DrawBitmap(ref Bitmap bmp, int x, int y , int w, int h)
+        {
+            mDevice.DrawImageUnscaled(bmp, x, y, w, h);
+        }
+        ~Game()
+        {
+
+        }
     }
 }
--- Practice/FrameWork/RPG/Sprite.cs
+++ Practice/FrameWork/RPG/Sprite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,79 @@ namespace RPG
 {
     class Sprite
     {
+        private Graphics mGraphics;
+        private int mCurrentFrame;
+        private int mTotalFrames;
+        private Bitmap[] mBitmaps;
+        private PointF mPos;
+        private bool isRunning;
+        public Sprite(Graphics graphics, Bitmap[] bitmaps = null)
+        {
+            mPos = new PointF(0, 0);
+            mBitmaps = bitmaps;
+            mGraphics = graphics;
+            mCurrentFrame = 0;
+            if (mBitmaps != null)
+            {
+                mTotalFrames = mBitmaps.Length;
+            }
+        }
+
+        public Bitmap[] Bitmaps
+        {
+            set
+            {
+                mBitmaps = value;
+            }
+        }
+
+        public float Y
+        {
+            get 
+            {
+                return mPos.Y;
+            }
+            set
+            {
+                mPos.Y = value;
+            }
+        }
+
+        public float X
+        {
+            get
+            {
+                return mPos.X;
+            }
+            set
+            {
+                mPos.X = value;
+            }
+        }
+
+        public void play()
+        {
+            isRunning = true;
+        }
+
+        public void stop()
+        {
+            isRunning = false;
+        }
+
+        public void update()
+        {
+            if (!isRunning)
+            {
+                return;
+            }
+            Bitmap bitmap = mBitmaps[mCurrentFrame];
+            mCurrentFrame++;
+            if (mCurrentFrame == mTotalFrames - 1)
+            {
+                mCurrentFrame = 0;
+            }
+            mGraphics.DrawImage(bitmap, mPos);
+        }
     }
 }
